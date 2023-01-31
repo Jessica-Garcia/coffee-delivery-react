8c3301de89585e73257e3abfcdadfbe216cb8a7b
Jessica-Garcia <jessica.rodriguesgarcia@gmail.com>
2023-01-31 20:20:22 -0300

    refactor: create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/theme/default";
-
+import { Router } from "./Router";
 export const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello World</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   );
