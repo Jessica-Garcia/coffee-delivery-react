6938c1582f986d3110ff5536f92aeb487f645c7c
Jessica-Garcia <jessica.rodriguesgarcia@gmail.com>
2023-02-09 20:27:45 -0300

    refactor: create products interface

---
+++ src/@types/Product.ts
@@ -0,0 +1,10 @@
+export interface Product {
+  id: number;
+  image: string;
+  name: string;
+  type: [];
+  // type: "TRADICIONAL" | "GELADO" | "COM LEITE" | "ESPECIAL" | "ALCOÓLICO";
+  description: String;
+  price: number;
+  quantity: number;
+}
