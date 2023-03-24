9f933374ff85fd977104f36d669e3fa0dc62903d
Jessica-Garcia <jessica.rodriguesgarcia@gmail.com>
2023-03-24 18:30:55 -0300

    refactor: Avoid redirection by url

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -9,7 +9,7 @@ import {
   Title,
   TitleAndSubtitleContainer,
 } from "./styles";
-
+import { Navigate } from "react-router-dom";
 import ImageSuccessPage from "../../assets/image-success-page.svg";
 import { CurrencyDollar, MapPin, Timer } from "phosphor-react";
 import { useContext } from "react";
@@ -17,53 +17,65 @@ import { ProductsContext } from "../../contexts/ProductsContext";
 
 export const Success = () => {
   const { newOrder } = useContext(ProductsContext);
+  const successPage = "/success";
+
   return (
-    <SuccessContainer>
-      <BannerContainer>
-        <OrderInfoContainer>
-          <TitleAndSubtitleContainer>
-            <Title>
-              <span>Uhu! Pedido confirmado</span>
-            </Title>
-            <Subtitle>
-              <span> Agora é só aguardar que logo o café chegará até você</span>
-            </Subtitle>
-          </TitleAndSubtitleContainer>
-          <OrderInfoContent>
-            <OderDetails iconsColor="purple">
-              <div>
-                <MapPin weight="fill" />
-              </div>
-              <span>
-                Entrega em{" "}
-                <strong>
-                  {newOrder?.street}, {newOrder?.number}{" "}
-                </strong>
-                {newOrder?.neighborhood} - {newOrder?.city}, {newOrder?.uf}
-              </span>
-            </OderDetails>
-            <OderDetails iconsColor="yellow">
-              <div>
-                <Timer weight="fill" />
-              </div>
-              <span>
-                Previsão de entrega <strong> 20 min - 30 min</strong>
-              </span>
-            </OderDetails>
-            <OderDetails iconsColor="yellowDark">
-              <div>
-                <CurrencyDollar weight="fill" />
-              </div>
-              <span>
-                Pagamento na entrega <strong>{newOrder?.paymentType}</strong>
-              </span>
-            </OderDetails>
-          </OrderInfoContent>
-        </OrderInfoContainer>
-        <ImgContainer>
-          <img src={ImageSuccessPage} alt="" />
-        </ImgContainer>
-      </BannerContainer>
-    </SuccessContainer>
+    <>
+      {window.location.pathname === successPage && !newOrder ? (
+        <Navigate to="/" />
+      ) : (
+        <SuccessContainer>
+          <BannerContainer>
+            <OrderInfoContainer>
+              <TitleAndSubtitleContainer>
+                <Title>
+                  <span>Uhu! Pedido confirmado</span>
+                </Title>
+                <Subtitle>
+                  <span>
+                    {" "}
+                    Agora é só aguardar que logo o café chegará até você
+                  </span>
+                </Subtitle>
+              </TitleAndSubtitleContainer>
+              <OrderInfoContent>
+                <OderDetails iconsColor="purple">
+                  <div>
+                    <MapPin weight="fill" />
+                  </div>
+                  <span>
+                    Entrega em{" "}
+                    <strong>
+                      {newOrder?.street}, {newOrder?.number}{" "}
+                    </strong>
+                    {newOrder?.neighborhood} - {newOrder?.city}, {newOrder?.uf}
+                  </span>
+                </OderDetails>
+                <OderDetails iconsColor="yellow">
+                  <div>
+                    <Timer weight="fill" />
+                  </div>
+                  <span>
+                    Previsão de entrega <strong> 20 min - 30 min</strong>
+                  </span>
+                </OderDetails>
+                <OderDetails iconsColor="yellowDark">
+                  <div>
+                    <CurrencyDollar weight="fill" />
+                  </div>
+                  <span>
+                    Pagamento na entrega{" "}
+                    <strong>{newOrder?.paymentType}</strong>
+                  </span>
+                </OderDetails>
+              </OrderInfoContent>
+            </OrderInfoContainer>
+            <ImgContainer>
+              <img src={ImageSuccessPage} alt="" />
+            </ImgContainer>
+          </BannerContainer>
+        </SuccessContainer>
+      )}
+    </>
   );
 };
